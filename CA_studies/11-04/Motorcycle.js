20e32af9a902ee4d0aded4aa45f2378cf58e8a56
JustinaNvr <justinanev@gmail.com>
2021-11-04 20:00:44 +0200

    11-04

--- CA_studies/11-04/Motorcycle.js
+++ CA_studies/11-04/Motorcycle.js
@@ -7,13 +7,6 @@ class Motorcycle extends Car {
     getIntroduction() {
         return `Motorcycles maker is '${this.make}', model of the motorcycle is '${this.model}'`
     };
-
-    getAge() {
-        let today = new Date().getFullYear();
-        let age = today - this.year;
-
-        return age <= 10 ? `${this.getIntroduction()}, it is made in ${this.year} and is 10 years old or newer` : `${this.getIntroduction()}, it is made in ${this.year} and is 11 years old or older`;
-    }
     getWheelsNumber() {
         if (this.wheels === 4){
             return `${this.getIntroduction()} and it is a quad bike`;
