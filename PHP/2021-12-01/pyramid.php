247c09ed25d220dc3aa472330af4b5b0f2006047
JustinaNvr <justinanev@gmail.com>
2021-12-01 20:31:38 +0200

    4th task

--- PHP/2021-12-01/pyramid.php
+++ PHP/2021-12-01/pyramid.php
@@ -14,7 +14,6 @@
 
 echo 1, '<br>', str_repeat(2, 2), '<br>', str_repeat(3, 3), '<br>', str_repeat(4, 4), '<br>', str_repeat(5, 5), '<br>', str_repeat(6, 6), '<br>',str_repeat(7, 7), '<br>', str_repeat(8, 8), '<br>', str_repeat(9, 9), '<br>';
 
-$arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 $i = 1;
 while ($i<10){
