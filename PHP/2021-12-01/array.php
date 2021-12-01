9cc9995e577f8c7d3c31a40dc181dd7e5c552f83
JustinaNvr <justinanev@gmail.com>
2021-12-01 19:59:43 +0200

    3rd task

--- PHP/2021-12-01/array.php
+++ PHP/2021-12-01/array.php
@@ -2,8 +2,8 @@
 
 //Sukurkite masyvą $arr iš reikšmių: '!', 'Hello', 'world'. Naudodami sukurtą masyvą priskirti reikšme 'Hello, world!' naujam kintamajam ir atspausdinkite jį ekrane.
 
-$arr = array('!', 'Hello', 'world');
+$arr = ['!', 'Hello', 'world'];
 
-$hello = $arr[1] . ', ' . $arr[2] . $arr[0];
+$hello = "{$arr[1]}, {$arr[2]}{$arr[0]}";
 
 echo "'$hello'";
