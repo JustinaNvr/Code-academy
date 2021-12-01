9a956304cbc899e0203278ec45b7454d4cfcd4e2
JustinaNvr <justinanev@gmail.com>
2021-12-01 19:36:06 +0200

    3rd task

---
+++ PHP/2021-12-01/array.php
@@ -0,0 +1,7 @@
+<?php
+
+//Sukurkite masyvą $arr iš reikšmių: '!', 'Hello', 'world'. Naudodami sukurtą masyvą priskirti reikšme 'Hello, world!' naujam kintamajam ir atspausdinkite jį ekrane.
+
+$arr = array('!', 'Hello', 'world');
+
+echo "'$arr[1], $arr[2]$arr[0]'";
