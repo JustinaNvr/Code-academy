e653e51224c84794911fcb8524a9b925536a3c75
JustinaNvr <justinanev@gmail.com>
2021-12-08 18:28:00 +0200

    12-07

--- PHP/2021-12-07/classworks/profile/profile.php
+++ PHP/2021-12-07/classworks/profile/profile.php
@@ -6,25 +6,26 @@ require_once 'profile_data.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PROFILE</title>
     <link rel="stylesheet" href="app.css">
 </head>
 <body>
     <div class="profile">
-    <div class="polaroid">
+        <h1>VARTOTOJO PROFILIS</h1>
+    <div class="picture">
         <img src="<?php echo $profilePicture; ?>">
     </div>
-
         <?php if (!empty($_POST['name'])): ?>
-            <h1><?php echo $name . '<br>' . $surname ?></h1>
-            <h2><?php echo $city ?></h2>
-            <h2><?php echo $prog_lan ?></h2>
-            <h2><?php echo $additional ?></h2>
+            <h2><?php echo $name . '<br>' . $surname ?></h2>
+            <h3><?php echo $city ?></h3>
+            <h3><?php echo $prog_lan ?></h3>
+            <h3><?php echo $additional ?></h3>
 
         <?php else: ?>
             <h3 style="color:red"> name not found</h3>
         <?php endif;?>
-        <button type="submit" onclick="history.go(-1);">Back</button>
+
+        <button type="submit" onclick="history.go(-1);">Kurti profilį iš naujo</button>
     </div>
 
 </body>
