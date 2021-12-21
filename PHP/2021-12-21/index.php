64924355b1b5ba181afefff825eea85f7202916f
JustinaNvr <justinanev@gmail.com>
2021-12-21 21:06:06 +0200

    12-21

--- PHP/2021-12-21/index.php
+++ PHP/2021-12-21/index.php
@@ -2,11 +2,11 @@
 // Panaudojus https://randomuser.me API parodykite vartotojo duomenis EXCEL programoje naudodami funkcijas skirtas darbui su failais
 
 
-$source = "https://randomuser.me/api/?format=csv";
+$source = 'https://randomuser.me/api/?format=csv';
 
 $data = file_get_contents($source);
 
-$myFile = fopen("data/user_.csv", "w");
+$myFile = fopen('data/user_.csv', 'w');
 
 fwrite($myFile, $data);
 
