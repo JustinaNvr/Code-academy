6e3d976d1604cb5cb4779d54827dd2d1904032b2
JustinaNvr <justinanev@gmail.com>
2021-12-21 21:03:22 +0200

    12-21

--- PHP/2021-12-21/data.php
+++ PHP/2021-12-21/data.php
@@ -1,52 +1,19 @@
 <?php
 
-$handle = fopen('https://randomuser.me/api/', 'r');
+$users = file_get_contents('https://randomuser.me/api/');
 
-if (FALSE === $handle) {
-    exit('Failed to open stream to URL');
-}
+file_put_contents('data/users.json', $users);
 
-$contents = '';
-
-$i = 0;
-
-while (!feof($handle)) {
-    $i++;
-
-    $chunk = fread($handle, 250);
-
-    $contents .= $chunk;
-
-    if (strpos($contents, "ga('create', 'UA-5421911-1', 'auto')") !== FALSE) {
-        var_dump($chunk);
-
-        break;
-    }
-}
-
-fclose($handle);
-
-file_put_contents('data/users.json', $contents);
-
-
-
-//An example JSON string.
 $jsonString = file_get_contents('data/users.json');
 
-//Decode the JSON and convert it into an associative array.
 $jsonDecoded = json_decode($jsonString, true);
 
-//Give our CSV file a name.
 $csvFileName = 'data/user.csv';
 
-//Open file pointer.
 $fp = fopen($csvFileName, 'w');
 
-//Loop through the associative array.
-foreach((array) $jsonDecoded as $row){
-    //Write the row to the CSV file.
+foreach($jsonDecoded as $row){
     fputcsv($fp, $row);
 }
 
-//Finally, close the file pointer.
 fclose($fp);
